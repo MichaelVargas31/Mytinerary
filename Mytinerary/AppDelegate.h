69127d57b074e85e571508caedb6ed89dd0aaf66
Michael Vargas <michaelvargas@fb.com>
2019-07-16 12:59:01 -0700

    Initial Commit

---
+++ Mytinerary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Mytinerary
+//
+//  Created by michaelvargas on 7/16/19.
+//  Copyright © 2019 michaelvargas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
