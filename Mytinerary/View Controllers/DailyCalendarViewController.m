b51b6f9e9c39eaa12e8aecc89254de36319166eb
Michael Vargas <michaelvargas@fb.com>
2019-07-16 17:17:12 -0700

    Table View with distinct times displayed

---
+++ Mytinerary/View Controllers/DailyCalendarViewController.m
@@ -0,0 +1,84 @@
+//
+//  DailyCalendarViewController.m
+//  Mytinerary
+//
+//  Created by michaelvargas on 7/16/19.
+//  Copyright © 2019 michaelvargas. All rights reserved.
+//
+
+
+/*
+ Brainstorming:
+    - Probably going to have increments of 15(?) minutes that are cells
+    - Use gesture recognizer to detect tap, which adds UIView to the screen?
+        - But can I add coordinates to the event to make sure it spans the timeframe?
+ 
+ 
+    Maybe NSCalendar could be useful?
+
+ 
+ Questions:
+    - Should formatter be defined as a property of the Daily View Controller?
+
+ */
+
+#import "DailyCalendarViewController.h"
+#import "DailyTableViewCell.h"
+
+@interface DailyCalendarViewController () <UITableViewDelegate, UITableViewDataSource>
+
+@end
+
+@implementation DailyCalendarViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+    
+    // initializing formatter for calculating cell's times
+    self.timeOfDayFormatter = [[NSDateFormatter alloc] init];
+    [self.timeOfDayFormatter setDateFormat:@"HH:mm:ss"];
+    self.tableView.rowHeight = 200;
+    
+}
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    
+    DailyTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"DailyEventCell" forIndexPath:indexPath];
+    
+    // Only for testing until we can actually take data and things
+    // self.eventArray = [];
+    NSDictionary *event = self.eventArray[indexPath.item];
+    
+    // set the cell labels with information from the event, then return;
+    // or should we only add that information later with content views(?)
+    NSDate *midnight = [self.timeOfDayFormatter dateFromString:@"00:00:00"];
+    NSDate *newTime = [midnight dateByAddingTimeInterval:1800*indexPath.row];
+    cell.calendarTimeLabel.text = [[self.timeOfDayFormatter stringFromDate:newTime] substringToIndex:5];
+
+    
+    return cell;
+
+
+}
+
+- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 48;
+}
+
+
+
+@end
---
+++ Mytinerary/Views/dailyTableViewCell.m
@@ -0,0 +1,27 @@
+//
+//  DailyTableViewCell.m
+//  Mytinerary
+//
+//  Created by michaelvargas on 7/16/19.
+//  Copyright © 2019 michaelvargas. All rights reserved.
+//
+
+#import "DailyTableViewCell.h"
+
+@implementation DailyTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+    self.rowHeight = [NSNumber numberWithInt:200];
+        // can be referenced when coordinating event overlaying, only have to change the height in one place
+    
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
