0c9fde5d0105053ec752429a46058e1441a9d16e
Emily Hong <ehhong@fb.com>
2019-07-23 14:30:57 -0700

    expanded event model to support latitude, longitude, endlatitude, endlongitude, locationtype

--- Mytinerary/Models/Event.m
+++ Mytinerary/Models/Event.m
@@ -18,6 +18,11 @@
 @dynamic foodCost;
 @dynamic foodType;
 @dynamic hotelType;
+@dynamic latitude;
+@dynamic longitude;
+@dynamic locationType;
+@dynamic endLatitude;
+@dynamic endLongitude;
 
 + (nonnull NSString *)parseClassName {
     return @"Event";
@@ -26,7 +31,7 @@
 /* initialize a event
 (required: title, category, start/end times, address, completion;
  optional: event description, notes) */
-- (void) initNewEvent:(NSString *)title eventDescription:(NSString * _Nullable)eventDescription address:(NSString * _Nullable)address category:(NSString *)category startTime:(NSDate *)startTime endTime:(NSDate *)endTime notes:(NSString * _Nullable)notes withCompletion:(PFBooleanResultBlock)completion {
+- (void) initNewEvent:(NSString *)title eventDescription:(NSString * _Nullable)eventDescription address:(NSString * _Nullable)address latitude:(NSNumber *)latitude longitude:(NSNumber *)longitude locationType:(NSString *)locationType category:(NSString *)category startTime:(NSDate *)startTime endTime:(NSDate *)endTime notes:(NSString * _Nullable)notes withCompletion:(PFBooleanResultBlock)completion {
     
     // make sure all required fields are filled, validate input
     if ([title isEqualToString:@""]) {
@@ -50,9 +55,13 @@
     self.category = category;
     self.startTime = startTime;
     self.endTime = endTime;
+    self.address = address;
+    self.latitude = latitude;
+    self.longitude = longitude;
+    self.locationType = locationType;
+    
     // optional fields
     self.eventDescription = eventDescription;
-    self.address = address;
     self.notes = notes;
     
     [self saveInBackgroundWithBlock:completion];
@@ -61,28 +70,28 @@
 /* initialize an activity event
  (required: title, category, start/end times, address, completion;
  optional: event description, cost, notes) */
-+ (void) initActivityEvent:(NSString *)title eventDescription:(NSString * _Nullable)eventDescription address:(NSString *)address startTime:(NSDate *)startTime endTime:(NSDate *)endTime cost:(float)cost notes:(NSString * _Nullable)notes withCompletion:(PFBooleanResultBlock)completion {
++ (void) initActivityEvent:(NSString *)title eventDescription:(NSString * _Nullable)eventDescription address:(NSString *)address latitude:(NSNumber *)latitude longitude:(NSNumber *)longitude locationType:(NSString *)locationType startTime:(NSDate *)startTime endTime:(NSDate *)endTime cost:(float)cost notes:(NSString * _Nullable)notes withCompletion:(PFBooleanResultBlock)completion {
     
     Event *event = [Event new];
     
     // activity specific field
     event.cost = @(cost);
     
-    [event initNewEvent:title eventDescription:eventDescription address:address category:@"activity" startTime:startTime endTime:endTime notes:notes withCompletion:completion];
+    [event initNewEvent:title eventDescription:eventDescription address:address latitude:latitude longitude:longitude locationType:locationType category:@"activity" startTime:startTime endTime:endTime notes:notes withCompletion:completion];
 }
 
 // update a preexisting activity event
-- (void) updateActivityEvent:(NSString *)title eventDescription:(NSString * _Nullable)eventDescription address:(NSString *)address startTime:(NSDate *)startTime endTime:(NSDate *)endTime cost:(float)cost notes:(NSString * _Nullable)notes withCompletion:(PFBooleanResultBlock)completion {
+- (void) updateActivityEvent:(NSString *)title eventDescription:(NSString * _Nullable)eventDescription address:(NSString *)address latitude:(NSNumber *)latitude longitude:(NSNumber *)longitude locationType:(NSString *)locationType startTime:(NSDate *)startTime endTime:(NSDate *)endTime cost:(float)cost notes:(NSString * _Nullable)notes withCompletion:(PFBooleanResultBlock)completion {
     // activity specific field
     self.cost = @(cost);
     
-    [self initNewEvent:title eventDescription:eventDescription address:address category:@"activity" startTime:startTime endTime:endTime notes:notes withCompletion:completion];
+    [self initNewEvent:title eventDescription:eventDescription address:address latitude:latitude longitude:longitude locationType:locationType category:@"activity" startTime:startTime endTime:endTime notes:notes withCompletion:completion];
 }
 
 /* initialize a transportation event
  (required: title, category, start/end times, start/end address, transportation type, completion;
  optional: event description, cost, notes) */
-+ (void) initTransportationEvent:(NSString *)title eventDescription:(NSString * _Nullable)eventDescription startAddress:(NSString *)startAddress endAddress:(NSString *)endAddress startTime:(NSDate *)startTime endTime:(NSDate *)endTime transpoType:(NSString *)transpoType cost:(float)cost notes:(NSString * _Nullable)notes withCompletion:(PFBooleanResultBlock)completion {
++ (void) initTransportationEvent:(NSString *)title eventDescription:(NSString * _Nullable)eventDescription startAddress:(NSString *)startAddress startLatitude:(NSNumber *)startLatitude startLongitude:(NSNumber *)startLongitude endAddress:(NSString *)endAddress endLatitude:(NSNumber *)endLatitude endLongitude:(NSNumber *)endLongitude startTime:(NSDate *)startTime endTime:(NSDate *)endTime transpoType:(NSString *)transpoType cost:(float)cost notes:(NSString * _Nullable)notes withCompletion:(PFBooleanResultBlock)completion {
     
     if ([startAddress isEqualToString:@""]) {
         NSError *error = [NSError errorWithDomain:@"Please add a start location" code:1 userInfo:nil];
@@ -100,13 +109,15 @@
     // transportation specific fields
     event.cost = @(cost);
     event.endAddress = endAddress;
+    event.endLatitude = endLatitude;
+    event.endLongitude = endLongitude;
     event.transpoType = transpoType;
     
-    [event initNewEvent:title eventDescription:eventDescription address:startAddress category:@"transportation" startTime:startTime endTime:endTime notes:notes withCompletion:completion];
+    [event initNewEvent:title eventDescription:eventDescription address:startAddress latitude:startLatitude longitude:startLongitude locationType:nil category:@"activity" startTime:startTime endTime:endTime notes:notes withCompletion:completion];
 }
 
 // update preexisting transportation event
-- (void) updateTransportationEvent:(NSString *)title eventDescription:(NSString * _Nullable)eventDescription startAddress:(NSString *)startAddress endAddress:(NSString *)endAddress startTime:(NSDate *)startTime endTime:(NSDate *)endTime transpoType:(NSString *)transpoType cost:(float)cost notes:(NSString * _Nullable)notes withCompletion:(PFBooleanResultBlock)completion {
+- (void) updateTransportationEvent:(NSString *)title eventDescription:(NSString * _Nullable)eventDescription startAddress:(NSString *)startAddress startLatitude:(NSNumber *)startLatitude startLongitude:(NSNumber *)startLongitude endAddress:(NSString *)endAddress endLatitude:(NSNumber *)endLatitude endLongitude:(NSNumber *)endLongitude startTime:(NSDate *)startTime endTime:(NSDate *)endTime transpoType:(NSString *)transpoType cost:(float)cost notes:(NSString * _Nullable)notes withCompletion:(PFBooleanResultBlock)completion {
     if ([startAddress isEqualToString:@""]) {
         NSError *error = [NSError errorWithDomain:@"Please add a start location" code:1 userInfo:nil];
         completion(nil, error);
@@ -121,15 +132,17 @@
     // transportation specific fields
     self.cost = @(cost);
     self.endAddress = endAddress;
+    self.endLatitude = endLatitude;
+    self.endLongitude = endLongitude;
     self.transpoType = transpoType;
     
-    [self initNewEvent:title eventDescription:eventDescription address:startAddress category:@"transportation" startTime:startTime endTime:endTime notes:notes withCompletion:completion];
+    [self initNewEvent:title eventDescription:eventDescription address:startAddress latitude:startLatitude longitude:startLongitude locationType:nil category:@"activity" startTime:startTime endTime:endTime notes:notes withCompletion:completion];
 }
 
 /* initialize a food event
  (required: title, category, start/end times, address, food cost, completion;
  optional: event description, food type, notes) */
-+ (void) initFoodEvent:(NSString *)title eventDescription:(NSString * _Nullable)eventDescription address:(NSString *)address startTime:(NSDate *)startTime endTime:(NSDate *)endTime foodType:(NSString *)foodType foodCost:(NSString *)foodCost notes:(NSString * _Nullable)notes withCompletion:(PFBooleanResultBlock)completion {
++ (void) initFoodEvent:(NSString *)title eventDescription:(NSString * _Nullable)eventDescription address:(NSString *)address latitude:(NSNumber *)latitude longitude:(NSNumber *)longitude locationType:(NSString *)locationType startTime:(NSDate *)startTime endTime:(NSDate *)endTime foodType:(NSString *)foodType foodCost:(NSString *)foodCost notes:(NSString * _Nullable)notes withCompletion:(PFBooleanResultBlock)completion {
     
     Event *event = [Event new];
     
@@ -137,40 +150,40 @@
     event.foodType = foodType;
     event.foodCost = foodCost;
     
-    [event initNewEvent:title eventDescription:eventDescription address:address category:@"food" startTime:startTime endTime:endTime notes:notes withCompletion:completion];
+    [event initNewEvent:title eventDescription:eventDescription address:address latitude:latitude longitude:longitude locationType:locationType category:@"food" startTime:startTime endTime:endTime notes:notes withCompletion:completion];
 }
 
 // update preexisting food event
-- (void) updateFoodEvent:(NSString *)title eventDescription:(NSString * _Nullable)eventDescription address:(NSString *)address startTime:(NSDate *)startTime endTime:(NSDate *)endTime foodType:(NSString *)foodType foodCost:(NSString *)foodCost notes:(NSString * _Nullable)notes withCompletion:(PFBooleanResultBlock)completion {
+- (void) updateFoodEvent:(NSString *)title eventDescription:(NSString * _Nullable)eventDescription address:(NSString *)address latitude:(NSNumber *)latitude longitude:(NSNumber *)longitude locationType:(NSString *)locationType startTime:(NSDate *)startTime endTime:(NSDate *)endTime foodType:(NSString *)foodType foodCost:(NSString *)foodCost notes:(NSString * _Nullable)notes withCompletion:(PFBooleanResultBlock)completion {
     
     // food specific fields
     self.foodType = foodType;
     self.foodCost = foodCost;
     
-    [self initNewEvent:title eventDescription:eventDescription address:address category:@"food" startTime:startTime endTime:endTime notes:notes withCompletion:completion];
+    [self initNewEvent:title eventDescription:eventDescription address:address latitude:latitude longitude:longitude locationType:locationType category:@"food" startTime:startTime endTime:endTime notes:notes withCompletion:completion];
 }
 
 /* initialize a hotel event
  (required: title, category, start/end times, address, hotel type, completion;
  optional: event description, cost, notes) */
-+ (void) initHotelEvent:(NSString *)title eventDescription:(NSString * _Nullable)eventDescription address:(NSString *)address startTime:(NSDate *)startTime endTime:(NSDate *)endTime hotelType:(NSString *)hotelType cost:(float)cost notes:(NSString * _Nullable)notes withCompletion:(PFBooleanResultBlock)completion {
++ (void) initHotelEvent:(NSString *)title eventDescription:(NSString * _Nullable)eventDescription address:(NSString *)address latitude:(NSNumber *)latitude longitude:(NSNumber *)longitude locationType:(NSString *)locationType startTime:(NSDate *)startTime endTime:(NSDate *)endTime hotelType:(NSString *)hotelType cost:(float)cost notes:(NSString * _Nullable)notes withCompletion:(PFBooleanResultBlock)completion {
     Event *event = [Event new];
     
     // hotel specific fields
     event.hotelType = hotelType;
     event.cost = @(cost);
     
-    [event initNewEvent:title eventDescription:eventDescription address:address category:@"hotel" startTime:startTime endTime:endTime notes:notes withCompletion:completion];
+    [event initNewEvent:title eventDescription:eventDescription address:address latitude:latitude longitude:longitude locationType:locationType category:@"hotel" startTime:startTime endTime:endTime notes:notes withCompletion:completion];
 }
 
 // update preexisting hotel event
-- (void) updateHotelEvent:(NSString *)title eventDescription:(NSString * _Nullable)eventDescription address:(NSString *)address startTime:(NSDate *)startTime endTime:(NSDate *)endTime hotelType:(NSString *)hotelType cost:(float)cost notes:(NSString * _Nullable)notes withCompletion:(PFBooleanResultBlock)completion {
+- (void) updateHotelEvent:(NSString *)title eventDescription:(NSString * _Nullable)eventDescription address:(NSString *)address latitude:(NSNumber *)latitude longitude:(NSNumber *)longitude locationType:(NSString *)locationType startTime:(NSDate *)startTime endTime:(NSDate *)endTime hotelType:(NSString *)hotelType cost:(float)cost notes:(NSString * _Nullable)notes withCompletion:(PFBooleanResultBlock)completion {
     
     // hotel specific fields
     self.hotelType = hotelType;
     self.cost = @(cost);
     
-    [self initNewEvent:title eventDescription:eventDescription address:address category:@"hotel" startTime:startTime endTime:endTime notes:notes withCompletion:completion];
+    [self initNewEvent:title eventDescription:eventDescription address:address latitude:latitude longitude:longitude locationType:locationType category:@"hotel" startTime:startTime endTime:endTime notes:notes withCompletion:completion];
 }
 
 
