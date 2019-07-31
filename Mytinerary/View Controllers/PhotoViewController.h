7e3a22118462d6f513a56154a278cbbad2fe06d6
Shandler Mason <samason1@fb.com>
2019-07-31 16:23:05 -0700

    camera and photo library view controller works properly

---
+++ Mytinerary/View Controllers/PhotoViewController.h
@@ -0,0 +1,17 @@
+//
+//  PhotoViewController.h
+//  Mytinerary
+//
+//  Created by samason1 on 7/31/19.
+//  Copyright © 2019 michaelvargas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PhotoViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
