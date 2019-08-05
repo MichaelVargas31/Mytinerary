d4aeaba7c4cb4cd310c12cc41f7c33f02dec3f47
Emily Hong <ehhong@fb.com>
2019-08-05 15:11:42 -0700

    added functionality to tap on title of map view --> itinerary details

--- Mytinerary/View Controllers/Location View Controllers/MapViewController.m
+++ Mytinerary/View Controllers/Location View Controllers/MapViewController.m
@@ -9,6 +9,7 @@
 #import <MapKit/MapKit.h>
 #import "MapViewController.h"
 #import "DailyCalendarViewController.h"
+#import "ItineraryDetailsViewController.h"
 #import "AppDelegate.h"
 #import "Parse/Parse.h"
 #import "Location.h"
@@ -151,8 +152,7 @@
 }
 
 - (void)onTapItineraryTitle {
-    // TODO -- wait for shandler to finish segues
-    NSLog(@"tapped itinerary title!");
+    [self performSegueWithIdentifier:@"mapToItineraryDetailsSegue" sender:nil];
 }
 
 #pragma mark - Transportation/directions stuff
@@ -245,6 +245,10 @@
     if ([[segue identifier] isEqualToString:@"mapToCalendarSegue"]) {
         SWRevealViewController *revealViewController = [segue destinationViewController];
         revealViewController.itinerary = self.itinerary;
+    }
+    else if ([[segue identifier] isEqualToString:@"mapToItineraryDetailsSegue"]) {
+        ItineraryDetailsViewController *itineraryDetailsViewController = [segue destinationViewController];
+        itineraryDetailsViewController.itinerary = self.itinerary;
     }
 }
 
