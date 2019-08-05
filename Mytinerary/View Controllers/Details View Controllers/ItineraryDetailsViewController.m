7957e32d5816672c9f42b8fecc50dd2929e3d115
Emily Hong <ehhong@fb.com>
2019-08-05 15:41:03 -0700

    sort events within each day in itinerary details table

--- Mytinerary/View Controllers/Details View Controllers/ItineraryDetailsViewController.m
+++ Mytinerary/View Controllers/Details View Controllers/ItineraryDetailsViewController.m
@@ -89,8 +89,15 @@ static const int TABLE_VIEW_HEADER_HEIGHT = 44;
                     [eventsByDay[itinDayIndex] addObject:event];
                 }
             }
-            self.eventsByDay = eventsByDay;
             
+            for (NSMutableArray <Event *>*dayEvents in eventsByDay) {
+                // sort events in ascending order by start time
+                [dayEvents sortUsingComparator:^NSComparisonResult(Event *event1, Event *event2) {
+                    return [event1.startTime compare:event2.startTime];
+                }];
+            }
+            
+            self.eventsByDay = eventsByDay;
             [self.tableView reloadData];
         }
         else {
