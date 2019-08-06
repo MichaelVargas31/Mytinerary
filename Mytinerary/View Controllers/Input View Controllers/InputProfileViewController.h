53c49cdb356b769f4dba008781c880cd8a155138
Emily Hong <ehhong@fb.com>
2019-08-06 09:17:22 -0700

    added IB outlets and edit profile view controller

---
+++ Mytinerary/View Controllers/Input View Controllers/InputProfileViewController.h
@@ -0,0 +1,17 @@
+//
+//  InputProfileViewController.h
+//  Mytinerary
+//
+//  Created by ehhong on 8/6/19.
+//  Copyright © 2019 michaelvargas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface InputProfileViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Mytinerary/View Controllers/Input View Controllers/InputProfileViewController.m
@@ -0,0 +1,43 @@
+//
+//  InputProfileViewController.m
+//  Mytinerary
+//
+//  Created by ehhong on 8/6/19.
+//  Copyright © 2019 michaelvargas. All rights reserved.
+//
+
+#import "InputProfileViewController.h"
+
+@interface InputProfileViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+@property (weak, nonatomic) IBOutlet UIPickerView *defaultItineraryPicker;
+
+@end
+
+@implementation InputProfileViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)onTapSubmitButton:(id)sender {
+}
+
+- (IBAction)onTapCloseButton:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
