ee9dd1ac69e0b033f661abb368a7c67c0cf4abaf
Michael Vargas <michael.vargas99@gmail.com>
2019-08-07 16:21:28 -0700

    Now resizing images, file still not saving to parse

--- Mytinerary/View Controllers/Input View Controllers/InputItineraryViewController.m
+++ Mytinerary/View Controllers/Input View Controllers/InputItineraryViewController.m
@@ -108,8 +108,11 @@
 
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
     // Get the image captured by the UIImagePickerController
-    UIImage *originalImage = info[UIImagePickerControllerOriginalImage];    
-    [self.imageView setImage:originalImage];
+//    UIImage *originalImage = info[UIImagePickerControllerOriginalImage];
+    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
+    
+    [self resizeImage:editedImage withSize:CGSizeMake(100, 100)];
+    [self.imageView setImage:editedImage];
     
     // Dismiss UIImagePickerController
     [self dismissViewControllerAnimated:YES completion:nil];
@@ -166,6 +169,25 @@
         }
     }
 }
+
+
+
+
+- (UIImage *)resizeImage:(UIImage *)image withSize:(CGSize)size {
+    UIImageView *resizeImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, size.width, size.height)];
+    
+    resizeImageView.contentMode = UIViewContentModeScaleAspectFill;
+    resizeImageView.image = image;
+    
+    UIGraphicsBeginImageContext(size);
+    [resizeImageView.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return newImage;
+}
+
+
 
 - (void)dismissKeyboard {
     [self.view endEditing:YES];
