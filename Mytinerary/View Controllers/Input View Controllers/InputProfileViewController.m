6f88a99fbe0f2acddc8cefb48bc2260c72fa78c5
Michael Vargas <michael.vargas99@gmail.com>
2019-08-08 17:02:30 -0700

    Added edit profile button to tab bar, segues successfully

--- Mytinerary/View Controllers/Input View Controllers/InputProfileViewController.m
+++ Mytinerary/View Controllers/Input View Controllers/InputProfileViewController.m
@@ -32,21 +32,42 @@
     [self.alert addAction:defaultAction];
     
     // prefill username/password text fields
+    // You have to fetch all the arrays with the author being this person
+    [self fetchUserItineraries];
     User *user = User.currentUser;
+
     self.usernameTextField.text = user.username;
 
     // fill in defaultItineraryPicker
     self.defaultItineraryPicker.delegate = self;
     self.defaultItineraryPicker.dataSource = self;
     
-    // prefill with current default Itinerary
-    if (user.defaultItinerary) {
-        NSLog(@"default itin: %@", user.defaultItinerary);
-        NSUInteger defaultItinIdx = [self.itineraries indexOfObjectPassingTest:^BOOL(Itinerary * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-            return [obj.objectId isEqualToString:user.defaultItinerary.objectId];
-        }];
-        [self.defaultItineraryPicker selectRow:defaultItinIdx inComponent:0 animated:NO];
-    }
+    
+}
+
+
+
+- (void) fetchUserItineraries {
+    PFQuery *query = [PFQuery queryWithClassName:@"Itinerary"];
+    [query whereKey:@"author" equalTo:User.currentUser];
+    [query findObjectsInBackgroundWithBlock:^(NSArray * _Nullable objects, NSError * _Nullable error) {
+        if (objects) {
+            self.itineraries = objects;
+            // prefill with current default Itinerary
+            User *user = User.currentUser;
+
+            if (user.defaultItinerary) {
+                NSLog(@"default itin: %@", user.defaultItinerary);
+                NSUInteger defaultItinIdx = [self.itineraries indexOfObjectPassingTest:^BOOL(Itinerary * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+                    return [obj.objectId isEqualToString:user.defaultItinerary.objectId];
+                }];
+                [self.defaultItineraryPicker selectRow:defaultItinIdx inComponent:0 animated:NO];
+                [self.defaultItineraryPicker reloadAllComponents];
+            }
+        } else {
+            NSLog(@"Error getting current user's itineraries: %@", error.localizedDescription);
+        }
+    }];
 }
 
 - (IBAction)onTapSubmitButton:(id)sender {
